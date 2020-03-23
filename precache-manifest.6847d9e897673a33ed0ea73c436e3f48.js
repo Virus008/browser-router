c31bbe2aa1a298d850892aa123af791ac580b929
virus008 <sonavalevrushabh@gmail.com>
2020-03-23 11:41:19 +0530

    Updates

---
+++ precache-manifest.6847d9e897673a33ed0ea73c436e3f48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d791a6285b60d03e4ec091233d9467ee",
+    "url": "/browser-router/index.html"
+  },
+  {
+    "revision": "96fceb135da49f0e41cd",
+    "url": "/browser-router/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "44a201c4622eb99b72a2",
+    "url": "/browser-router/static/js/2.212ac3c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/browser-router/static/js/2.212ac3c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96fceb135da49f0e41cd",
+    "url": "/browser-router/static/js/main.1fe08fff.chunk.js"
+  },
+  {
+    "revision": "783ae986292e486353f4",
+    "url": "/browser-router/static/js/runtime-main.b93eed73.js"
+  }
+]);
